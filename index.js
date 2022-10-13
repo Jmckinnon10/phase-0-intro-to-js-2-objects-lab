128f73d73ad616671c78fd82a7340db21c0c60d5
John McKinnon <johnmckinnon23@yahoo.com>
2022-10-13 14:11:27 -0700

    commit tree 46cacb892006e8c7127abb1806d7c794e2331a84

--- index.js
+++ index.js
@@ -1,1 +1,87 @@
-// Write your solution in this file!
+// /* Function should take in 3 arguments 
+// employee object a key and a value*/
+const employee = {
+  name: 'John',
+  streetAddress: '64 Steven st' 
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmployee = {...employee}
+  newEmployee[key] = value
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = {...employee}
+  delete newEmployee[key]
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+  delete employee[key];
+  return employee;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const employee = {
+//   name: "John",
+//   streetAddress: "64 Steven st, Hanson MA",
+// }
+
+
+// function updateEmployeeWithKeyAndValue(employee, key, value){
+//   const newEmployee = {...employee}
+
+//   newEmployee[key] = value;
+//   return newEmployee;
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+//   employee[key] = value;
+//   return employee;
+// }
+
+// function deleteFromEmployeeByKey(employee, key){
+//   const newKey = {...key};
+//   delete newKey[key];
+//   return newKey;
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key){
+//   const deleteEmployee = {...employee};
+//   delete employee[key];
+//   return employee;
+// }
